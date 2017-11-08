8ee8f63d6b15999f2e13fd5d27c898fdff5caad1
bdx_inc <bdxinc.contact@gmail.com>
2017-11-08 01:24:33 -0800

    added function alertContents
    added function alertContents

--- bdx_random_user_generator.js
+++ bdx_random_user_generator.js
@@ -1,3 +1,5 @@
+(function() {
+
 function generateUser(event) {
   if (event.target !== event.currentTarget && event.target.nodeName === "BUTTON") {
     console.log(event.target.name);
@@ -5,5 +7,23 @@ function generateUser(event) {
   event.stopPropagation();
 }
 
+httpRequest.onreadystatechange = generateUser;
+
+httpRequest.open('GET', 'https://randomuser.me/api', true);
+httpRequest.send();
+
+function alertContents() {
+  if (httpRequest.readySate === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      var response = JSON.parse(httpRequest.responseText);
+      alert(response.computedString)
+    } else {
+      alert('There was a problem with the request.');
+    }
+  }
+}
+
 var buttonsEl = document.getElementById("buttons");
 buttonsEl.addEventListener("click", generateUser, true);
+
+})();
