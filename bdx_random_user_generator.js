d1e186d551f8cc7d10a351d89ba5c2622dcf034b
bdx_inc <bdxinc.contact@gmail.com>
2017-11-09 00:44:11 -0800

    setup ajax xhr
    setup ajax xhr

--- bdx_random_user_generator.js
+++ bdx_random_user_generator.js
@@ -1,29 +1,46 @@
 (function() {
 
-function generateUser(event) {
-  if (event.target !== event.currentTarget && event.target.nodeName === "BUTTON") {
-    console.log(event.target.name);
+  // function generateUser(event) {
+  //   if (event.target !== event.currentTarget && event.target.nodeName === "BUTTON") {
+  //     console.log(event.target.name);
+  //   }
+  //   event.stopPropagation();
+  // }
+
+
+
+  var httpRequest;
+  var buttonsEl = document.getElementById("xhr");
+  buttonsEl.addEventListener("click", makeRequest, true);
+
+  function makeRequest() {
+    httpRequest = new XMLHttpRequest();
+    console.log(httpRequest);
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', 'https://randomuser.me/api');
+    httpRequest.send();
   }
-  event.stopPropagation();
-}
 
-httpRequest.onreadystatechange = generateUser;
 
-httpRequest.open('GET', 'https://randomuser.me/api', true);
-httpRequest.send();
-
-function alertContents() {
-  if (httpRequest.readySate === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) {
-      var response = JSON.parse(httpRequest.responseText);
-      alert(response.computedString)
-    } else {
-      alert('There was a problem with the request.');
+  function alertContents() {
+    try {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        console.log(httpRequest.readyState);
+        if (httpRequest.status === 200) {
+          console.log(httpRequest.status);
+          var response = JSON.parse(httpRequest.responseText);
+          alert(response.computedString)
+        } else {
+          alert('There was a problem with the request.');
+        }
+      }
+    }
+      catch( e ) {
+        alert('Caught Exception: ' + e.description);
     }
   }
-}
-
-var buttonsEl = document.getElementById("buttons");
-buttonsEl.addEventListener("click", generateUser, true);
-
 })();
