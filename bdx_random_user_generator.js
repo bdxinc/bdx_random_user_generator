30760a4b4a69636b5b1ca379f076fc3596a6b9e8
bdx_inc <bdxinc.contact@gmail.com>
2017-11-11 02:07:51 -0800

    set up fetch, axios, & jQuery requests
    set up fetch, axios, & jQuery requests

--- bdx_random_user_generator.js
+++ bdx_random_user_generator.js
@@ -1,46 +1,100 @@
 (function() {
 
-  // function generateUser(event) {
-  //   if (event.target !== event.currentTarget && event.target.nodeName === "BUTTON") {
-  //     console.log(event.target.name);
-  //   }
-  //   event.stopPropagation();
-  // }
+  function handleClick(event) {
+    if (event.target !== event.currentTarget && event.target.nodeName === "BUTTON") {
+      console.log(event.target.name);
+
+      generateUser(event.target.name);
+    }
+    event.stopPropagation();
+  }
+
+  var buttonsEl = document.getElementById("buttons");
+  buttonsEl.addEventListener("click", handleClick, true);
+
+  function generateUser(callType) {
+    switch(callType) {
+      case "xhr":
+        xhrRequest();
+        break;
+      case "fetch":
+        fetchRequest();
+        break;
+      case "axios":
+        axiosRequest();
+        break;
+      case "jQuery":
+        jQueryRequest();
+        break;
+    }
+  }
 
 
-
-  var httpRequest;
-  var buttonsEl = document.getElementById("xhr");
-  buttonsEl.addEventListener("click", makeRequest, true);
-
-  function makeRequest() {
-    httpRequest = new XMLHttpRequest();
+  function xhrRequest() {
+    var httpRequest = new XMLHttpRequest();
     console.log(httpRequest);
     if (!httpRequest) {
-      alert('Giving up :( Cannot create an XMLHTTP instance');
+      console.log('Giving up :( Cannot create an XMLHTTP instance');
       return false;
     }
-    httpRequest.onreadystatechange = alertContents;
+    httpRequest.onreadystatechange = function() {
+      try {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          console.log(httpRequest.readyState);
+          if (httpRequest.status === 200) {
+            console.log(httpRequest.status);
+            var response = JSON.parse(httpRequest.responseText);
+            console.log(response.results[0]);
+
+            displayUser(response.results[0]);
+
+          } else {
+            console.log('There was a problem with the request.');
+          }
+        }
+      }
+        catch( e ) {
+          console.log('Caught Exception: ' + e.description);
+      }
+    };
     httpRequest.open('GET', 'https://randomuser.me/api');
     httpRequest.send();
   }
 
-
-  function alertContents() {
-    try {
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        console.log(httpRequest.readyState);
-        if (httpRequest.status === 200) {
-          console.log(httpRequest.status);
-          var response = JSON.parse(httpRequest.responseText);
-          alert(response.computedString)
-        } else {
-          alert('There was a problem with the request.');
-        }
-      }
-    }
-      catch( e ) {
-        alert('Caught Exception: ' + e.description);
-    }
+  function fetchRequest() {
+    fetch('https://randomuser.me/api').then(function(response) {
+      return response.json();
+    }).then(function(myJSON) {
+      console.log(myJSON.results[0]);
+      displayUser(myJSON.results[0]);
+    })
   }
+
+  function axiosRequest() {
+    axios.get('https://randomuser.me/api').then(function(response) {
+      console.log(response.data.results[0]);
+      displayUser(response.data.results[0]);
+    });
+  }
+
+  function jQueryRequest() {
+    $.get('https://randomuser.me/api', function(data) {
+      console.log(data.results[0]);
+      displayUser(data.results[0]);
+    });
+  }
+
+
+  function displayUser(userInfo) {
+    // work-in-progress
+    document.getElementById('fullName').textContent = userInfo.name.first;
+    // document.getElementById('').textContent = response.results[0].;
+    // document.getElementById('').textContent = response.results[0].;
+    // document.getElementById('').textContent = response.results[0].;
+    // document.getElementById('').textContent = response.results[0].;
+    // document.getElementById('').textContent = response.results[0].;
+    // document.getElementById('').textContent = response.results[0].;
+  }
+
+
 })();
